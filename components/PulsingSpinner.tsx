34a6f86c4eea554e47d4a373a79b36301f5273a6
myguel raton <myguelraton@gmail.com>
2025-08-27 14:35:15 -0700

    move spinner higher up

--- components/PulsingSpinner.tsx
+++ components/PulsingSpinner.tsx
@@ -15,7 +15,7 @@ export default function PulsingSpinner({ ipc }: PulsingSpinnerPops) {
     return null;
 
   return (
-    <div className="absolute flex items-center justify-center top-1/2 left-1/2 transform -translate-x-1/3 -translate-y-1/3 z-20">
+    <div className="absolute flex items-center justify-center top-1/3 left-1/2 transform -translate-x-1/3 -translate-y-1/3 z-20">
       <div className="w-16 h-16 flex justify-center items-center space-x-2">
         <div className="w-4 h-4 bg-blue-400 rounded-full animate-pulse"></div>
         <div className="w-4 h-4 bg-blue-500 rounded-full animate-pulse delay-200"></div>
